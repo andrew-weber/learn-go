fe7283afb0ece45fd5e162a27719b626f5eebc9b
Andrew Weber <weber.andrew1@gmail.com>
2024-01-12 11:51:06 -0500

    fix: render tmpl with echo

--- templ-example/main.go
+++ templ-example/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	web "web/hello"
+	web "templ-exampl/web"
 
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
 
+func renderHtml(cmp templ.Component) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+		return cmp.Render(c.Request().Context(), c.Response().Writer)
+	}
+}
+
 func main() {
 	e := echo.New()
 
-	component := web.Hello("John")
+	hello := web.Hello("John")
+	test := web.Test("asdfasdf")
 
-	e.GET("/", templ.Handler(component))
+	e.GET("/", renderHtml(hello))
+	e.GET("/1", renderHtml(test))
+
+	// e.GET("/")
 	e.Logger.Fatal(e.Start(":3000"))
 }
